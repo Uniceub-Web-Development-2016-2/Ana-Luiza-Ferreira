2aa3811f976141eb7407f51f78750ca22d667bdb
Ana Luiza <analuiza.ferreiraa@hotmail.com>
2016-11-29 16:04:06 -0200

    Alterando

--- Atelie/Atelie/Atelie/Cliente/user/request.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('httpful.phar');
-$get_request = 'http://localhost/Atelie/user/login/search?cpf_user="'.$_GET['search'].'"';
-$response = \Httpful\Request::get($get_request)->send();
-echo  $response->body;
-?>
--- Atelie/Servidor/control/resource_controller.php
+++ Atelie/Servidor/control/resource_controller.php
@@ -1,58 +1,49 @@
 <?php
 
-include_once ('./model/request.php');
-include_once ('./control/db_manager.php');
+include_once ('.Atelie/Servidor/model/request.php');
+include_once ('.Atelie/Servidor/control/db_manager.php');
 
 class ResourceController
-
 {	
  	private $METHODMAP = ['GET' => 'search' , 'POST' => 'create' , 'PUT' => 'update', 'DELETE' => 'remove' ];
 	
 	public function treat_request($request) {
-		//var_dump($request);
-		//die();
-		if($request->getMethod() == "POST" && $request->getOperation() == "login")
-		{	
+		//var_dump($request);	
+		if($request->getMethod() == "POST" && $request->getOperation() == "login"){
 			return $this->login($request);
 		}
 		return $this->{$this->METHODMAP[$request->getMethod()]}($request);
 	
 	}
 	public function login($request) {
-		//var_dump($request->getResource);
-		$query = 'SELECT * FROM '.$request->getResource().' WHERE '.self::bodyParams($request->getBody());
+		//var_dump($request->getResource() );
+		$query = 'SELECT * FROM '. $request->getResource().' WHERE '.self::bodyParams($request->getBody());
+		//var_dump($query);
 		$result = (new DBConnector())->query($query); 
-                return $result->fetchAll(PDO::FETCH_ASSOC);
+		return $result->fetchAll(PDO::FETCH_ASSOC);
 		
-	}
+	}	
 	private function search($request) {
 		$query = 'SELECT * FROM '.$request->getResource().' WHERE '.self::queryParams($request->getParameters());
-		// var_dump($query);
-		$result = (new DBConnector())->query($query);
+		$result = (new DBConnector())->query($query); 
 		return $result->fetchAll(PDO::FETCH_ASSOC);
-		//return $query; 
 	}
-	private function select($query){
-		$connect = (new DBConnector())->query($query);
-		//var_dump($query);
-		return $connect->fetchAll(PDO::FETCH_ASSOC);
-        	return $connect;
-	}
-	private function update($request) {
+	
+	private function create($request) {
 		$body = $request->getBody();
 		$resource = $request->getResource();
-		$query = 'UPDATE '.$resource.' SET '. $this->getUpdateCriteria($body);
-		//var_dump($query);
-		return self::execution_query($query);
-        }
-	private function create($request) {		
-		$body = $request->getBody();
-		//var_dump($body);
-		$resource = $request->getResource();		
 		$query = 'INSERT INTO '.$resource.' ('.$this->getColumns($body).') VALUES ('.$this->getValues($body).')';
-		return self::execution_query($query);
+		return (new DBConnector())->query($query);
 		 
 	}
+	
+	private function update($request) {
+                $body = $request->getBody();
+                $resource = $request->getResource();
+                $query = 'UPDATE '.$resource.' SET '. $this->getUpdateCriteria($body);
+		return $query;
+      }
+	
 	private function bodyParams($json) {
 		$criteria = "";
                 $array = json_decode($json, true);
@@ -62,37 +53,7 @@ class ResourceController
                 }
                 return substr($criteria, 0, -5);
 	}
-		
-	private function queryParams($params) {
-		$query = "";		
-		foreach($params as $key => $value) {
-			$query .= $key.' = '."'".$value."'".' AND ';	
-		}	
-		
-		$query = substr($query,0,-5);
-		
-		if($params != null){				
-			$query = "";		
-			foreach($params as $key => $value) {
-				$query .= $key." = '".$value."' AND ";	
-			}
-			$query = substr($query,0,-5);
-			return $query;
-		}else{
-			return 1;
-		}
-	}
-	private function execution_query($query){
-		$conn = (new DBConnector());
-		//$conn->query($query);
-		//echo ($query);
-		if ($conn->query($query) == TRUE) {
-                	echo "New record created successfully!";
-        	} else {
-                	echo "Error: " . $query . "<br>";
-        	}
-	}
-		
+	
 	private function getUpdateCriteria($json){
 		$criteria = "";
 		$where = " WHERE ";
@@ -102,21 +63,34 @@ class ResourceController
 				$criteria .= $key." = '".$value."',";
 			
 		}
-		return substr($criteria, 0, -1).$where." id = '".$array['id']."'";
-	}
-	
+		return substr($criteria, 0, -1).$where." id = ".$array['id'];
+	}	
 	private function getColumns($json){
 		$array = json_decode($json, true);
 		$keys = array_keys($array);
 		return implode(",", $keys);
 	
 	}
-	private function getValues($json){
+	private function getValues($json) 
+        {
                 $array = json_decode($json, true);
-                $values = array_values($array);
-                $string = implode("','", $values);
+                $keys = array_values($array);
+                $string =  implode("','", $keys);
 		return "'".$string."'";
         
         }
+	private function queryParams($params) {
+		$query = "";		
+		foreach($params as $key => $value) {
+			$query .= $key;
+			if(is_numeric($value)){
+				$query .=" = '".$value."'"." AND ";
+			}else{
+				$query .=" LIKE '%".$value."%'"." AND ";
+			}
+		}
+		$query = substr($query,0,-5);
+		return $query;
+	}
 	
 }
